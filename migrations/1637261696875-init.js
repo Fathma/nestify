5a01ab94aa81c5a1dafa99068b1e5a0b7a0260b3
FathmaSiddique <siddiquefathma@gmail.com>
2021-11-19 22:07:44 +0600

    changed migration file

--- migrations/1637261696875-init.js
+++ migrations/1637261696875-init.js
@@ -1,13 +1,42 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+const { MigrationInterface, QueryRunner } = require('typeorm');
 
 module.exports = class init1637261696875 {
-    name = 'init1637261696875'
+  name = 'init1637261696875';
 
-    async up(queryRunner) {
-        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "name" varchar NOT NULL, "age" integer NOT NULL)`);
-    }
+  async up(queryRunner) {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'age',
+            type: 'integer',
+          },
+        ],
+      })
+    );
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-}
+  async down(queryRunner) {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
+};
